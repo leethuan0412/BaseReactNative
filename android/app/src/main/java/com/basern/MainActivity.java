7639b3b5e172e7903bc66492fa414b3f901bb409
duc.dao <duc.daominh@plancare.com.au>
2023-02-12 15:05:31 +0700

    create Splashscreen

--- android/app/src/main/java/com/basern/MainActivity.java
+++ android/app/src/main/java/com/basern/MainActivity.java
@@ -1,9 +1,9 @@
 package com.basern;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,7 +14,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "BaseRN";
   }
-
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
--- src/navigation/navigator/AppNavigator.tsx
+++ src/navigation/navigator/AppNavigator.tsx
@@ -2,9 +2,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {AuthStackComponent} from '../stack/AuthStack';
 import {MainStackComponent} from '../stack/MainStack';
-
+import SplashScreen from 'react-native-splash-screen'
+import { useEffect } from 'react';
 const NavigationApp = React.forwardRef((props, ref: any) => {
   const token = true;
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
 
   const renderStackApp = () => {
     if (!token) {
